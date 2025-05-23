084f94b790237822d5b4e5d3c5d5c14a101a67fa
sumin <mins19960091@gmail.com>
2025-05-23 11:16:15 +0900

    CBT 연습

---
+++ src/Compare.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Compare {
+    /*두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if ( A >= -10000 && A <= 10000 && B >= -10000 && B <= 10000){
+        } if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
+
